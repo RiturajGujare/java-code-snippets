ad357314fe1b05342b31ee45a724818d903fd555
Rituraj Gujare <riturajgujare01@gmail.com>
2025-09-14 14:05:18 +0530

    created unique folders

--- src/arrays/FindNSmallestIntegers.java
+++ src/arrays/FindNSmallestIntegers.java
@@ -1,8 +1,8 @@
+package arrays;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class FindNSmallestIntegers {
@@ -12,16 +12,19 @@ public class FindNSmallestIntegers {
         //Given is the Integer list
         List<Integer> listOfIntegers = Arrays.asList(12, 23, 33, 45, 54, 12, 90, 34, 9, 25);
 
+        //Take the input to find n smallest integers
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number to find smallest integers: ");
         int n = sc.nextInt();
 
-        List<Integer> nthList = listOfIntegers.stream()
-                                .sorted(Comparator.reverseOrder())
-                                .distinct()
-                                .limit(n)
-                                .collect(Collectors.toList());
+        List<Integer> nthList = listOfIntegers.stream()  //stream the list
+                                .sorted(Comparator.reverseOrder())  //sort in reverse order
+                                .distinct()  //take all the unique integers
+                                .limit(n)   //limit only till nth integer
+                                .collect(Collectors.toList());   //collect the list
 
+        
+        // print the list 
         System.out.println("List of " + n + " smallest integers: " + nthList);
 
         sc.close();
--- src/arrays/SecondHighestElement.java
+++ src/arrays/SecondHighestElement.java
@@ -1,3 +1,4 @@
+package arrays;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
