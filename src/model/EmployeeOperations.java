c1025c9d289f700934b4aa058448244248b1cf75
Rituraj Gujare <riturajgujare01@gmail.com>
2025-09-19 21:00:00 +0530

    Add logic to find highest salaried employee using sorting and Comparator

--- src/model/EmployeeOperations.java
+++ src/model/EmployeeOperations.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -30,6 +31,8 @@ public class EmployeeOperations {
                                 .average()
                                 .orElse(0);
         System.out.println("Average salary of employees is: " + averageSalary);
+        System.out.println("-----------------------------------");
+        System.out.println("--------------------------------");
 
         //Filtering employees who earn more than the average salary
         //Using Stream API to filter employees based on their salary compared to the average salary
@@ -44,6 +47,8 @@ public class EmployeeOperations {
             System.out.println("Name of Employee: " + employee.getName() + " and his salary is: " + employee.getSalary());
         }
 
+        System.out.println("-----------------------------------");
+        System.out.println("--------------------------------");
         //snippet to filter employees by department
         Scanner sc = new Scanner(System.in);   //Scanner object to take user input
         System.out.println("Enter the department whose employee are to be fetched: ");
@@ -58,6 +63,8 @@ public class EmployeeOperations {
             System.out.println("Employee name: " + emp.getName() + " works in " + emp.getDepartment() + " department"); //printing employee name and department
         }
 
+        System.out.println("-----------------------------------");
+        System.out.println("--------------------------------");
 
         //Sorting employees based on their salary in ascending order using Stream API
         List<Employee> salaryBasedSortedEmployee = employees.stream()  //creating stream of Employee objects
@@ -73,6 +80,8 @@ public class EmployeeOperations {
         }
                                                     
 
+        System.out.println("-----------------------------------");
+        System.out.println("--------------------------------");
         //Grouping employees based on their department using Stream API
         Map<String, List<Employee>> groupedEmployee = employees.stream()  //creating stream of Employee objects
                                                         .collect(Collectors.groupingBy(
@@ -89,6 +98,28 @@ public class EmployeeOperations {
         });
 
 
+        System.out.println("-----------------------------------");
+        System.out.println("--------------------------------");
+
+        //example to find the employee with the highest salary using Stream API
+        //Using sorted() method to sort employees based on salary in descending order and then finding the first employee in the sorted list
+        //Also using max() method with Comparator to find the employee with the highest salary
+        Employee employee = employees.stream()
+                            //sorting employees based on salary in descending order, e2.getSalary() is compared with e1.getSalary() because we want the highest salary first
+                            .sorted((e1, e2) -> Double.compare(e2.getSalary(), e1.getSalary())) 
+                            .findFirst()
+                            .orElse(null);
+        System.out.println("Highest salaried employee is: " + employee.getName() + " and his salary is: " + employee.getSalary());
+
+        //Using max() method with Comparator to find the employee with the highest salary
+        //comparing employee salary using comparingDouble method of Comparator
+        Employee employee2 = employees.stream()
+                            .max(Comparator.comparingDouble(Employee::getSalary))
+                            .orElse(null);
+        System.out.println("Highest salaried employee calculated using comparingDouble method is: " + employee2.getName() + " and his salary is: " + employee2.getSalary());
+
+        System.out.println("-----------------------------------");
+        System.out.println("--------------------------------");
 
         sc.close(); //closing the scanner object to prevent resource leak
     }
